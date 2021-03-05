13371fd6c5bf595a72b8d40b6094eeb202cacaed
Daphaz <bonnet.damien@ymail.com>
2021-03-05 17:23:57 +0100

    feat: contact form
    create own validation for first and lastname

--- src/routes/contact/index.jsx
+++ src/routes/contact/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Layout } from "../../components/Layout";
 import { Title } from "../../components/Title";
+import { FormContact } from "../../components/FormContact";
 
 const Contact = ({ history, nbProduit }) => {
 	return (
 		<Layout history={history} nbProduit={nbProduit}>
-			<Title title="contact" subtitle="us" />
+			<Title title="nous" subtitle="contacter" />
+			<div className="container">
+				<FormContact />
+			</div>
 		</Layout>
 	);
 };
